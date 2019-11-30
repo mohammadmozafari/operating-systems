ceed93cac670fdbbc038bc0e0b6fca5b0b1f11e8
mohammad mozafari <mozafari.mmd@gmail.com>
2019-11-30 04:22:20 -0800

    3.5.1 - OriginalSchedTest completed

--- xv6-public-master/OriginalSchedTest.c
+++ xv6-public-master/OriginalSchedTest.c
@@ -3,55 +3,54 @@
 
 int main()
 {
-    const int fork_num = 2;
-    int fid[fork_num], i;
-    int root = 1;
+    const int fork_num = 10;
+    int i;
     struct timeVariables *t[fork_num];
     int childrenId[fork_num];
     int ct, tt, st, rt, rut;
+	int sum_tt = 0, sum_wt = 0, sum_ct = 0;
+	int avg_tt, avg_wt, avg_ct;
+
+	changePolicy(1);
 
     for (i = 0; i < fork_num; i++)
         t[i] = (struct timeVariables *)malloc(sizeof(struct timeVariables));
 
     for (i = 0; i < fork_num; i++)
     {
-        fid[i] = fork();
-        if (fid[i] < 0)
-            return 1;
+        if (fork() == 0)
+        {
+            for (i = 0; i < 1000; i++)
+                printf(1, "%d: %d\n", getpid(), (i+1));
+            exit();
+        }
     }
+    for (i = 0; i < fork_num; i++)
+        childrenId[i] = waitForChild(t[i]);
 
-    for (i = 0; i < 10; i++)
-        printf(1, "%d: %d\n", getpid(), (i+1));
-
+	printf(1, "\n");
     for (i = 0; i < fork_num; i++)
     {
-        if (fid[i] == 0)
+        if (childrenId[i] != -1)
         {
-            root = 0;
-            break;
+            ct = t[i]->creationTime;
+            tt = t[i]->terminationTime;
+            st = t[i]->sleepingTime;
+            rt = t[i]->readyTime;
+            rut = t[i]->runningTime;
+            printf(1, "process %d -> created: %d - terminated: %d - slept: %d - ready: %d - running: %d\n", childrenId[i], ct, tt, st, rt, rut);
+            printf(1, "T.T: %d, W.T: %d, CBT: %d\n", (tt - ct), (tt - ct - rut), rut);
+            printf(1, "====================================================================================\n");
+			sum_tt += tt - ct;
+			sum_wt += tt - ct - rut;
+			sum_ct += rut;
         }
     }
-    if (root == 1)
-    {
-        for (i = 0; i < fork_num; i++)
-        {
-            childrenId[i] = waitForChild(t[i]);
-        }
+	avg_tt = sum_tt / fork_num;
+	avg_wt = sum_wt / fork_num;
+	avg_ct = sum_ct / fork_num;
+	printf(1, "\nAvg T.T: %d, Avg W.T: %d, Avg CBT: %d\n", avg_tt, avg_wt, avg_ct);
+	printf(1, "=========================================\n");
 
-        for (i = 0; i < fork_num; i++)
-        {
-            if (childrenId[i] != 0)
-            {
-                ct = t[i]->creationTime;
-                tt = t[i]->terminationTime;
-                st = t[i]->sleepingTime;
-                rt = t[i]->readyTime;
-                rut = t[i]->runningTime;
-                printf(1, "%d : %d %d %d %d %d\n", childrenId[i], ct, tt, st, rt, rut);
-                printf(1, "T.T: %d, W.T: %d, CBT: %d\n", (tt - ct), (tt - ct - rut), rut);
-                printf(1, "=========================\n");
-            }
-        }
-    }
-
+	return 0;
 }
